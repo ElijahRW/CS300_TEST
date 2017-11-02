8510041efcd00c999da04c8a9f46064c939f0179
Elijah Rich-Wimmer <elij2@pdx.edu>
2017-11-02 16:20:52 -0700

    Basic Player inventory implementation

---
+++ display_inventory.cpp
@@ -0,0 +1,19 @@
+//This program is designed to write the player's inventory to the user interface
+//Considerations include: inventory, energy, currency, and numeric location
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+
+int main ()
+{
+	//basic formatting setup for the html output
+	cout << "Content-Type: text/html;charset=us-ascii\n\n";
+	cout <<"Testing inventory materials" << endl;
+	return 0;
+}
