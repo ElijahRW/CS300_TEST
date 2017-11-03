92feb991e21dd601d7b4c2f3de702fecb430bf21
Elijah Rich-Wimmer <elij2@pdx.edu>
2017-11-03 13:30:00 -0700

    Additional reworks

--- display_inventory.cpp
+++ display_inventory.cpp
@@ -9,11 +9,26 @@
 
 using namespace std;
 
-
+void get_player_info(const char * filename);
+void write_player_info(/*we should create  a player class to contain this information*/);
 int main ()
 {
+	int energy = 0;
+	int 
 	//basic formatting setup for the html output
 	cout << "Content-Type: text/html;charset=us-ascii\n\n";
 	cout <<"Testing inventory materials" << endl;
 	return 0;
+}
+
+void get_player_info(const char * filename)
+{
+	
+	
+}
+
+void write_player_info()
+{
+	
+	
 }
